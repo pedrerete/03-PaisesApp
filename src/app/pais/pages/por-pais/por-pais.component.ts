ec6f60e10e142c85094ab3242fbd57b60487de63
Pedro Esparza <p_mauricio@hotmail.com>
2022-06-03 16:58:58 -0500

    componente por pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  termino: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  buscar(){
+    console.log(this.termino);
+    this.termino = "";
   }
 
 }
