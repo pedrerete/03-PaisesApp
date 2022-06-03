2e9e9021f2224835e4a5cb6185b3711d33922731
Pedro Esparza <p_mauricio@hotmail.com>
2022-06-02 20:25:23 -0500

    routermodule y sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
@@ -11,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    PaisModule
+    PaisModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
