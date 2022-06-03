5570690936df57c71e9208c62a9acdcf9b44a3cc
Pedro Esparza <p_mauricio@hotmail.com>
2022-06-02 19:13:49 -0500

    PaisesApp primera version

---
+++ src/app/pais/pais.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorRegionComponent,
+    PorPaisComponent,
+    VerPaisComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PorCapitalComponent,
+    PorRegionComponent,
+    PorPaisComponent,
+    VerPaisComponent
+  ]
+})
+export class PaisModule { }
